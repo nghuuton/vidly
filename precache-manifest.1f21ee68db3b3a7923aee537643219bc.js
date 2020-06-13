72733f4fd109d833576c9ceee36e8c68fd4937dd
Nguyen Huu Ton <nghuuton@gmail.com>
2020-06-13 13:53:58 +0700

    Updates

---
+++ precache-manifest.1f21ee68db3b3a7923aee537643219bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17db69fca39fb47863b9ea9b5efbc9fa",
+    "url": "/vidyl/index.html"
+  },
+  {
+    "revision": "62fcda2e3aea018e4a84",
+    "url": "/vidyl/static/css/2.d590e795.chunk.css"
+  },
+  {
+    "revision": "7a893a6f037124ba726b",
+    "url": "/vidyl/static/css/main.b96d6e5b.chunk.css"
+  },
+  {
+    "revision": "62fcda2e3aea018e4a84",
+    "url": "/vidyl/static/js/2.7edeb8f0.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/vidyl/static/js/2.7edeb8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a893a6f037124ba726b",
+    "url": "/vidyl/static/js/main.8073b6cd.chunk.js"
+  },
+  {
+    "revision": "414a12e3230ddcc18932",
+    "url": "/vidyl/static/js/runtime-main.64b2931e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidyl/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidyl/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidyl/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidyl/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidyl/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
